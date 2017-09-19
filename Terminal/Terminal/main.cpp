1d97c9ac4229cc957635372f22a0e5c723207324
Chudleyj <chudleyjustin@gmail.com>
2017-09-19 17:33:42 -0400

    Add files via upload
    Changed echo and cd to be all read from same line like actual terminal does instead of old "enter a target directory" etc statements after typing cd or echo. Added template for ls function (ls does nothing yet)

--- Terminal/Terminal/main.cpp
+++ Terminal/Terminal/main.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 #include <string>
 #include <unistd.h> //Allows for getcwd
-#include <cstring> //Allows for strtok
+#include <sstream> //Used for splitting strings
 
 bool GLOBAL_EXIT = 0; //Using this in order to have exec_exit properly close program by ending the while loop in main
 
@@ -22,12 +22,8 @@ void exec_help(int arr_size, std::string curr_cmds[])
     }
 }
 
-void exec_echo()
+void exec_echo(std::string echo_input)
 {
-    std::string echo_input;
-    std::cout << "Enter echo statement." << std::endl << ">";
-    std::cin.ignore(1000, '\n'); //Clears the buffer of leftover inputs so getline will read the echo.
-    std::getline (std::cin, echo_input);
     std::cout << ">" << echo_input << std::endl;
 }
 
@@ -53,15 +49,9 @@ void exec_pwd()
     std::cout << ptr << std::endl;
 }
 
-void exec_cd()
+void exec_cd(std::string destination)
 {
-    int BUFFER_SIZE = 1024;
-    char *directory;
-    directory = new char [BUFFER_SIZE]; //Allocated memory in order to cin to the pointer char
-    std::cout << "Enter target directory: " << std::endl << ">";
-    std::cin >> directory;
-    chdir(directory);
-    delete [] directory;
+    chdir(destination.c_str());
 }
 
 void exec_error()
@@ -69,34 +59,73 @@ void exec_error()
     std::cout << "Error: command not found." << std::endl;
 }
 
+void exec_ls()
+{
+    
+}
+
 std::string get_command()
 {
-    std::string input_str;
-    char input[] = "";
+    std::string input;
     std::cout << ">";
-    std::cin >> input;
-    return std::string(input);
+    std::getline(std::cin, input);
+    return input;
+}
+
+std::string trim(std::string str)
+{
+    size_t first = str.find_first_not_of(' ');
+    if (std::string::npos == first)
+    {
+        return str;
+    }
+    size_t last = str.find_last_not_of(' ');
+    return str.substr(first, (last - first + 1));
+}
+
+std::string splitString(std::string str)
+{
+    std::string split;
+    std::istringstream iss{str};
+    iss >> split;
+    
+    std::ostringstream oss;
+    oss << iss.rdbuf();
+    split = oss.str();
+    return split;
+}
+
+std::string get_first(std::string str)
+{
+    std::string first_word = str.substr(0, str.find(" "));
+    return first_word;
 }
 
 void read_cmd(std::string cmd)
 {
-    int ARR_SIZE = 5; //Manually update this as commands are added, used for passing to the help func
-    std::string current_commands[] = {"help","echo","exit","pwd","cd"};
+    std::string first_cmd = get_first(cmd); //This way the actual command is seperate from extra words (e.g. echo "this")
+    std::string after_first = splitString(cmd); //Used to split the string into the first word and the rest
+    after_first = trim(after_first);
+    int NUM_COMMANDS = 6; //Manually update this as commands are added, used for passing to the help func
+    std::string current_commands[] = {"help","echo","exit","pwd","cd", "ls"};
     
-    if(cmd == current_commands[0])
-        exec_help(ARR_SIZE, current_commands);
+    if(first_cmd == current_commands[0])
+        exec_help(NUM_COMMANDS, current_commands);
     
-    else if(cmd == current_commands[1])
-        exec_echo();
+    else if(first_cmd == current_commands[1])
+        exec_echo(after_first);
     
-    else if(cmd == current_commands[2])
+    else if(first_cmd == current_commands[2])
         exec_exit();
     
-    else if(cmd == current_commands[3])
+    else if(first_cmd == current_commands[3])
         exec_pwd();
     
-    else if(cmd == current_commands[4])
-        exec_cd();
+    else if(first_cmd == current_commands[4])
+        exec_cd(after_first);
+    
+    else if(first_cmd == current_commands[5])
+        exec_ls();
     
     else
         exec_error();
@@ -104,8 +133,7 @@ void read_cmd(std::string cmd)
 
 void terminal_loop()
 {
-    std::string cmd;
-    cmd = get_command();
+    std::string cmd = get_command();
     read_cmd(cmd);
 }
 int main(int argc, const char * argv[]) {
